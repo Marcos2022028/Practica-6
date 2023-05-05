67c2a4e81b4a1f8d0c4868ee4b20d52e3fe87f24
Marcos2022028 <130589561+Marcos2022028@users.noreply.github.com>
2023-05-05 17:47:54 -0600

    Update Practica_6_.ino

--- Practica_6_.ino
+++ Practica_6_.ino
@@ -1,69 +1,69 @@
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
-#include <DallasTemperature.h>
-#include <RTClib.h>
+#include <Wire.h> //Libreria de I2C
+#include <LiquidCrystal_I2C.h>//Libreria I2C LCD
+#include <DallasTemperature.h>//Libreri del sensor de temperatura
+#include <RTClib.h>//Libreria del reloj
 
-#define lcdAddress 0x27  
-#define filas 2
-#define columnas 16
-LiquidCrystal_I2C pantallalcd(lcdAddress, columnas, filas);
+#define lcdAddress 0x27  //Se define acceso del Lcd
+#define filas 2//Se definen las filas
+#define columnas 16//Se definen las columnas
+LiquidCrystal_I2C pantallalcd(lcdAddress, columnas, filas);//Se colocan los define usados anteriormente 
 
-OneWire ourWire(6);
-DallasTemperature sensors(&ourWire);
+OneWire ourWire(6);//Se coloca el pin del rtc
+DallasTemperature sensors(&ourWire);//Declara el sensor de temperatura
 
-RTC_DS3231 rtc;
+RTC_DS3231 rtc;//se le da nombre al rtc
 
-#define SIN 5
-#define salidabuzzer() pinMode(SIN,OUTPUT)
-#define encenbuzzer() tone(SIN,1000)
-#define apagadobuzzer() noTone(SIN)
+#define SIN 5//Defien el pin del Buzzer
+#define outbuzzer() pinMode(SIN,OUTPUT)//salida del buzzer
+#define encenbuzzer() tone(SIN,1000)//Encen del buzz
+#define apagbuzzer() noTone(SIN)//Apag buzz
 
-#define JUAN 2
-#define salidabomba() pinMode(JUAN,OUTPUT)
-#define encenderbomba() digitalWrite(JUAN,HIGH)
-#define apagarbomba() digitalWrite(JUAN,LOW)
+#define JUAN 2//Se define pin de la bomba
+#define outbomba() pinMode(JUAN,OUTPUT)//Salida de la bomba
+#define encenbomba() digitalWrite(JUAN,HIGH)//Encen de la bomba
+#define apagbomba() digitalWrite(JUAN,LOW)//Apag de la bomba
 
 void setup() {
-  pantallalcd.init();
-  pantallalcd.backlight();
+  pantallalcd.init();//Iniciador de la lcd
+  pantallalcd.backlight();//Iniciador de la luz del lcd
   sensors.begin();
   rtc.begin();
-  rtc.adjust(DateTime(__DATE__,__TIME__));
-  salidabuzzer();
-  salidabomba();
+  rtc.adjust(DateTime(__DATE__,__TIME__));//Se inicia la fecha y el tiempo
+  outbuzzer();//Salida del buzzer
+  outbomba();//Salida de la bomba
 
 }
 
 void loop() {
   sensors.requestTemperatures();
-  float temperatura = sensors.getTempCByIndex(0);
-  pantallalcd.setCursor(0,0);  
-  pantallalcd.print(temperatura);
-  pantallalcd.print("°C ");
+  float temperatura = sensors.getTempCByIndex(0);//Se usa la variable para grados celcius
+  pantallalcd.setCursor(0,0);  //Inicio del cursor
+  pantallalcd.print(temperatura);//Se coloca temperatura en la lcd
+  pantallalcd.print(" C ");//Se coloca EL SIGNO
   
-  DateTime fecha = rtc.now();
-  pantallalcd.print(  fecha.hour());
+  DateTime fecha = rtc.now();//Se coloca fecha
+  pantallalcd.print(  fecha.hour());//Uso de la hora
   pantallalcd.print(":");				
-  pantallalcd.print(fecha.minute());
+  pantallalcd.print(fecha.minute());//Uso de la fecha 
   
-  delay(200);
+  delay(200);//Tiempo estipulado
 
-  if (fecha.hour() == 12 && fecha.minute() == 12 && fecha.second() == 0){    
-    encenbuzzer();
-    pantallalcd.setCursor(0,1);
-    pantallalcd.print("   Inicia en  ");
-    delay(5000);
-    pantallalcd.clear();
+  if (fecha.hour() == 12 && fecha.minute() == 12 && fecha.second() == 0){//Se inicia bucle con la hora y minutos y el segundo    
+    encenbuzzer();//Se enciende el buzzer
+    pantallalcd.setCursor(0,1);//Donde inicia el cursor
+    pantallalcd.print("   Inicia en  ");//Lo que se enseña en el lcd
+    delay(5000);//tiempo
+    pantallalcd.clear();//Se limpia la Lcd
     
   } 
-  if (fecha.hour() == 12 && fecha.minute() == 12 && fecha.second() == 11) {
-  apagadobuzzer();
-  pantallalcd.setCursor(0,1);
-  pantallalcd.print("   Hora de regar   ");
-  apagarbomba();
-  delay(20000);
-  pantallalcd.clear();
+  if (fecha.hour() == 12 && fecha.minute() == 12 && fecha.second() == 11) {//Se inicia bucle con la hora y minutos y el segundo   
+  apagbuzzer();//Se apaga buzzer
+  pantallalcd.setCursor(0,1);//Cursor del lcd
+  pantallalcd.print("   Hora de regar   ");///Lo que se enseña en el lcd
+  apagbomba();//Se apaga la bomba
+  delay(20000);//Tiempo de encendido 
+  pantallalcd.clear();//Se lipia la pantalla
   } else{
-    encenderbomba();
+    encenbomba();//Se apaga la bomba
   }
 }
